887aca20569d62eae4d702a19f0a228a528491ff
azqa <axka.asad@gmail.com>
2023-07-04 15:40:31 -0400

    azqa: room and utlties

--- src/Utilities.js
+++ src/Utilities.js
@@ -1,22 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, props } from 'react';
 
 
-function Temperature() {
-    const [temperatureValue, setTemperatureValue] = useState(70);
+class Temperature extends React.Component {
+
+   
+    constructor(props){
+        super(props);
+    }
+
+    increase = (temperature) => {
+        const p = this.props.temp;
+        this.props.onTempChange(p+1);
+    }
+
+    decrease = (temperature) => {
+        const p = this.props.temp;
+        this.props.onTempChange(p-1);
+    }
+
+    render() {
+        //const [temperatureValue, setTemperatureValue] = useState(70);
+        const movado = this.props.temp;
     return (
         <div class='temp-body'>
             <div class='app-container'>
                 <div class='temperature-display-container'>
-				    <div className='temperature-display'>{temperatureValue}°F</div>
+				    <div className='temperature-display'>{movado}°F</div>
                 </div>
                 <div class='button-div'>
-                    <button onClick={() => setTemperatureValue(temperatureValue + 1)}><b>+</b></button>
-                    <button onClick={() => setTemperatureValue(temperatureValue - 1)}><b>-</b></button>
+                    <button onClick={() => this.increase(movado)}><b>+</b></button>
+                    <button onClick={() => this.decrease(movado)}><b>-</b></button>
                 </div>
             </div>
         </div>
     );
 }
+}
+
 export default Temperature;
 
 
