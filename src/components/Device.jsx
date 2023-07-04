5de2f78bb080d2aac306c0a90f70ab7c7d47ebea
wiqabl2 <waleed.iqbal2001@gmail.com>
2023-07-04 15:54:56 -0400

    waleed: schedule in device

--- src/components/Device.jsx
+++ src/components/Device.jsx
@@ -1,9 +1,9 @@
 
-
 import '../styles.css';
 import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
+
 
 // Initial state of devices
 const initialDeviceStates = {
@@ -36,7 +36,32 @@ function Device() {
           {isOn ? 'On' : 'Off'}
         </Button>
       </div>
+      <div className="container mt-5">
+      <div className="text-center">
+        <h1 className="select">Selected Device:{deviceName}</h1>
+      </div>
+      <div className="mt-4 text-center">
+        <h2 className="font-weight-bold">Start Date & Time:</h2>
+        <input
+          type="datetime-local"
+          className="datepicker1"
+          style={{ width: '300px',height: '40px' }}
+        />
+      </div>
+      <div className="mt-5 text-center">
+        <h3 className="font-weight-bold">End Date & Time:</h3>
+        <input
+          type="datetime-local"
+          className="datepicker2"
+          style={{ width: '300px' ,height: '40px'}}
+        />
+      </div>
+      <div className="text-center mt-5">
+        <button className="btn btn-primary btn-lg">SAVE</button>
+      </div>
     </div>
+    </div>
+    
   );
 }
 //7/3/23
