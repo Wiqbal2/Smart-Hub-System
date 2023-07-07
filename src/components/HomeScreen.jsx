1d6a8eebae8ee8fcbf1c269054eaf180a97aa452
yusufsiddiqui1 <yusufs1@umbc.edu>
2023-07-07 10:38:49 -0400

    Yusuf: Added weather to widget

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -6,6 +6,8 @@ function HomeScreen() {
     const { deviceStatuses } = useContext(DeviceContext);
     const activeDevices = Object.keys(deviceStatuses).filter(device => deviceStatuses[device]);
 
+    const [weatherData, setWeatherData] = useState(null);
+
     // Function to update date and time
     const updateTime = () => {
         setDateTime(new Date());
@@ -15,6 +17,18 @@ function HomeScreen() {
     useEffect(() => {
         const interval = setInterval(updateTime, 1000);
         return () => clearInterval(interval); // Clear interval on component unmount
+    }, []);
+
+    // Function to fetch weather data
+    useEffect(() => {
+        const getWeatherData = async () => {
+            const response = await fetch(
+                'https://api.open-meteo.com/v1/forecast?latitude=39.29&longitude=-76.61&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch'
+            );
+            const data = await response.json();
+            setWeatherData(data.current_weather.temperature);
+        };
+        getWeatherData();
     }, []);
 
     return (
@@ -41,6 +55,7 @@ function HomeScreen() {
                 <div className="date-time">
                     <p>{dateTime.toLocaleDateString()}</p>
                     <p>{dateTime.toLocaleTimeString()}</p>
+                    {weatherData && <p>Current Weather: {weatherData}°F</p>}
                 </div>
                 <div className="active-devices">
                     <table className="table table-hover">
